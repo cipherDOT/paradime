7e581c06b3375d26d8c10f5fd9e2bc349dabecdd
Kavin Bharathi <66783110+cipherDOT@users.noreply.github.com>
2022-07-13 10:41:02 +0530

    Update settings.py

--- paradime/settings.py
+++ paradime/settings.py
@@ -146,6 +146,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
